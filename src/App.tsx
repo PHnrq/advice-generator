ee5fd9f8e61fa80b86a8087b20a9654bdb60bf6c
PHnrq <paulofigueredo7@gmail.com>
2022-04-11 11:36:40 -0300

    install and confugure axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { api } from "./services/api";
+
+interface AdvicesProps {
+  id: number,
+  advice: string,
+}
 
 export function App() {
+
+  const [advices, setAdvices] = useState<AdvicesProps>({'advice': '', 'id': 0});
+
+  useEffect(() => {
+    api.get('/advice').then(response => setAdvices(response.data.slip));
+    
+  }, [])
+
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <p>{advices.id}</p>
+      <p>{advices.advice}</p>
     </div>
   );
 }
