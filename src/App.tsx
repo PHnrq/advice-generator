c83c1e4d8c948d03ea4f08dec74961d3092f5461
PHnrq <paulofigueredo7@gmail.com>
2022-04-13 08:32:31 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "./services/api";
-
-interface AdvicesProps {
-  id: number,
-  advice: string,
-}
+import { GlobalStyle } from "./style/GlobalStyle";
+import { AdviceCard } from "./components/AdviceCard";
 
 export function App() {
-
-  const [advices, setAdvices] = useState<AdvicesProps>({'advice': '', 'id': 0});
-
-  useEffect(() => {
-    api.get('/advice').then(response => setAdvices(response.data.slip));
-    
-  }, [])
-
   return (
-    <div className="App">
-      <p>{advices.id}</p>
-      <p>{advices.advice}</p>
-    </div>
+    <section>
+      <GlobalStyle />
+      <AdviceCard />
+    </section>
   );
 }
