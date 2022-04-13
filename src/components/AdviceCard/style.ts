47018d6c7fb71ba1d7b578821b4b61f1b46dd72f
PHnrq <paulofigueredo7@gmail.com>
2022-04-13 10:15:34 -0300

    Update advice style

--- src/components/AdviceCard/style.ts
+++ src/components/AdviceCard/style.ts
@@ -1,23 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    position: relative;
+    max-width: 675px;
+    padding: 5.6rem 8.6rem;
+    border-radius: 3rem;
+
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background: var(--grayish-blue);
-    padding: 3.2rem;
-    border-radius: 0.8rem;
+    gap: 3.2rem;
 
+    background:  var(--grayish-blue);
+    
     font-size: 2.8rem;
     font-weight: 800;
+    text-align: center;
 
     & p:nth-child(1) {
         color: var(--neon-green);
-        margin-bottom: 1.6rem;
     }
 
     & p:nth-child(2) {
         color: #fff;
     }
-    `;
+
+    @media (max-width: 675px) {
+        max-width: 375px;
+    }
+`;
